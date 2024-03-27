866cab06c5d83b160dada8904e9dd910c8687d52
RonaldoSC14 <rcserna09@gmail.com>
2024-03-26 20:27:42 -0700

    Destroy Out of Bounds
    Added a code in the destroy out of bounds script to show when the game is over.

--- SernaUnit2-BasicGamePlayP#5/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ SernaUnit2-BasicGamePlayP#5/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    float lastKeyPressTime = 0;
+    float cooldown = 1;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Time.time - lastKeyPressTime > cooldown)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastKeyPressTime = Time.time;
+            }
         }
     }
 }
--- SernaUnit2-BasicGamePlayP#5/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ SernaUnit2-BasicGamePlayP#5/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22.0f;
-    private float spawnLimitXRight = 7.0f;
-    private float spawnPosY = 30.0f;
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
     private float spawnInterval;
@@ -32,9 +32,9 @@ public class SpawnManagerX : MonoBehaviour
         int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         //Generate random spawn position 
-        Vector3 spawnPos = new Vector3(Random.(0, spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
+        // Instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
